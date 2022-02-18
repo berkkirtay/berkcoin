1de77fb3d6e52dc250035c16de6b4202ab73ea90
Berk Kırtay <berkberk24@gmail.com>
2022-02-18 21:23:56 +0300

    Updates

---
+++ precache-manifest.764064f5b0fd9cce99b7f5add73ba87d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a265ef9ff705ff506d555330d0aec43",
+    "url": "/berkcoin/index.html"
+  },
+  {
+    "revision": "19eba8bc9a49ecf2f863",
+    "url": "/berkcoin/static/css/main.252eff83.chunk.css"
+  },
+  {
+    "revision": "f873e9616cdbb34e0a9a",
+    "url": "/berkcoin/static/js/2.c74b5127.chunk.js"
+  },
+  {
+    "revision": "19eba8bc9a49ecf2f863",
+    "url": "/berkcoin/static/js/main.a2cb971d.chunk.js"
+  },
+  {
+    "revision": "19325a9b26cbef82050b",
+    "url": "/berkcoin/static/js/runtime-main.7506f390.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70c0508b5e1019756bb039c9cf7112fa.js"
+  "/berkcoin/precache-manifest.764064f5b0fd9cce99b7f5add73ba87d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/berkcoin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
