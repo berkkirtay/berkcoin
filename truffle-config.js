b9d32cfbc5cc879247551dc381d00dbc0447cbe7
Berk Kırtay <berkberk24@gmail.com>
2022-02-24 01:03:39 +0300

    Truffle config files are arrenged for tests

--- truffle-config.js
+++ truffle-config.js
@@ -1,31 +1,10 @@
 const path = require("path");
-try {
-  let { projectId, mnemonic } = require('./secrets.json');
-}
-catch (err) {
-  projectId = 0;
-  mnemonic = "null";
-  console.log(err);
-}
-
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
       port: 7545
-    },
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    },
+    }
   },
 
   compilers: {
@@ -37,7 +16,6 @@ module.exports = {
           enabled: true,
           runs: 200
         },
-        //  evmVersion: "byzantium"
       }
     }
   }
